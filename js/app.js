51f5c2f2e6451af4be2288074b18d35256f00582
Shanthi Karunakaran <shanthi.arun@gmail.com>
2015-04-24 12:07:34 -0700

    added semi colons to functions and randomized enemy speed and pos

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var score = 0;
 
 // Enemies our player must avoid
-var Enemy = function (x, y, speed) {
+var Enemy = function (x) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -10,8 +10,13 @@ var Enemy = function (x, y, speed) {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
-    this.y = y;
-    this.speed = speed;
+    var EnemyPosY = [75, 160, 240],
+        EnemySpeed = [100, 200, 300, 400, 500];
+
+    //randomize the y enemy position
+    this.y = EnemyPosY[Math.floor(Math.random() * EnemyPosY.length)];
+    //randomize the enemy speed
+    this.speed = EnemySpeed[Math.floor(Math.random() * EnemySpeed.length)];
 };
 
 // Update the enemy's position, required method for game
@@ -27,66 +32,65 @@ Enemy.prototype.update = function (dt) {
         //Enemy is back to the init position
         this.x = -10;
     }
-}
+};
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 //Player is the constructor to which you add properties using the "this" keyword
-var Player = function(x,y,speed) {
+var Player = function (x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
     //array to hold all the Player char. images
-    var PlayerChars = ['images/char-boy.png','images/char-cat-girl.png','images/char-horn-girl.png','images/char-pink-girl.png','images/char-princess-girl.png'];
+    var PlayerChars = ['images/char-boy.png', 'images/char-cat-girl.png', 'images/char-horn-girl.png', 'images/char-pink-girl.png', 'images/char-princess-girl.png'];
     //randomize the Player image characters
     this.sprite = PlayerChars[Math.floor(Math.random() * PlayerChars.length)];
 };
 
 //update for player class
-Player.prototype.update = function() {
+Player.prototype.update = function () {
     // when the player reaches the water tile, then the game has to reset
     if (this.y < 60) {
         this.playerWon();
         this.reset();
     }
     this.updateScore(score);
-}
+};
 
-Player.prototype.updateScore = function(score) {
+Player.prototype.updateScore = function (score) {
     document.getElementById('score').innerHTML = 'You score: ' +score;
-}
+};
 
-Player.prototype.resetScore = function() {
+Player.prototype.resetScore = function () {
     score = 0;
     this.updateScore(score);
-}
+};
 
 //Draw the player on the screen, required method for game
-Player.prototype.render = function() {
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 //Method to increment score once the player reaches the tile without getting hit by the bug
-Player.prototype.playerWon = function() {
+Player.prototype.playerWon = function () {
     score += 10;
-
-}
+};
 
 //Method to decrement score when the player gets hit
-Player.prototype.playerHit = function() {
+Player.prototype.playerHit = function () {
    if (score > 0) {
         score -= 10;
     }
-}
+};
 
 //colWidth = 101 , rowHeight = 83 (x counts from left to right , y counts from top to bottom)
-Player.prototype.handleInput = function(key) {
+Player.prototype.handleInput = function (key) {
     //respond to keypress events only when the game starts(gamePlaying is a flag set inside the game object)
     if(game.gamePlaying == true) {
         if (key === 'left' && this.x > 101) {
@@ -99,13 +103,13 @@ Player.prototype.handleInput = function(key) {
             this.y += 83;
         }
     }
-}
+};
 
 //Reset the player to its starting position once the character is hit by the bugs
-Player.prototype.reset = function() {
+Player.prototype.reset = function () {
     this.x = 5;
     this.y = 410;
-}
+};
 
 //Create the object literal for the Game object (Constructor and Prototype not required since this is a Singleton object, no multiple instances of the object)
 var game = {
@@ -124,13 +128,13 @@ var game = {
         btnElem.innerHTML = "Restart the game";
         btnElem.disabled = true;
     },
-    startTimer: function(secs,elem) {
+    startTimer: function (secs, elem) {
         //run the timer once every second(1000 ms)
         var timer = setTimeout(function(){
             game.countdown(secs,elem);
         },1000);
     },
-    countdown: function(secs,elem) {
+    countdown: function (secs, elem) {
         //status element
         var element = document.getElementById(elem);
         var btnElem = document.getElementById("startBtn");
@@ -144,10 +148,10 @@ var game = {
             element.innerHTML = "Sorry,Game is over.";
             game.gameEnd();
         } else {
-            game.startTimer(secs,elem);
+            game.startTimer(secs, elem);
         }
     },
-    gameEnd: function() {
+    gameEnd: function () {
          //Emtpy the Array to stop the bugs from moving since game is over
         allEnemies = [];
         //bring the player back to the original position
@@ -156,7 +160,7 @@ var game = {
         var btnElem = document.getElementById("startBtn");
         btnElem.innerHTML = "Restart the game";
         btnElem.disabled = false;
-        btnElem.onclick = function() {
+        btnElem.onclick = function () {
             game.gameStart(btnElem);
         }
     }
@@ -169,10 +173,10 @@ var player = new Player(5,410,15);
 var initxEnemy = -10;
 
 //Instantiate the enemy objects
-var enemy1 = new Enemy(initxEnemy,40,50);
-var enemy2 = new Enemy(initxEnemy,90,100);
-var enemy3 = new Enemy(initxEnemy,100,150);
-var enemy4 = new Enemy(initxEnemy,130,130);
+var enemy1 = new Enemy(initxEnemy);
+var enemy2 = new Enemy(initxEnemy);
+var enemy3 = new Enemy(initxEnemy);
+var enemy4 = new Enemy(initxEnemy);
 //Intialize the array
 var allEnemies = [];
 
